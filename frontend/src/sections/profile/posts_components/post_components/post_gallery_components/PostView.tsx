88bafbe607c54bd09fc360047f3d518aa12c05b1
matejabodanovic <bogdanovicmateja75@gmail.com>
2025-05-02 00:40:20 +0200

    Comment scroll fix.

--- frontend/src/sections/profile/posts_components/post_components/post_gallery_components/PostView.tsx
+++ frontend/src/sections/profile/posts_components/post_components/post_gallery_components/PostView.tsx
@@ -3,37 +3,21 @@ import PostHeader from "../PostHeader";
 import { useState } from "react";
 import LikeButton from "../LikeButton";
 import ShareButton from "../ShareButton";
+import Comments from "./post_view_components/Comments";
 
 const PostView = ({ hideOverlay }: { hideOverlay?: Function }) => {
   const post: PostContextType = usePostContext();
   if (!post) return;
   const [actionsShown, setActionsShown] = useState<boolean>(true);
-
-  const [startY, setStartY] = useState<number | null>(null);
   const [swiped, setSwiped] = useState<"up" | "down">("down");
-  console.log("a");
-  const handleTouchStart = (e: React.TouchEvent) => {
-    setStartY(e.touches[0].clientY);
-  };
-
-  const handleTouchEnd = (e: React.TouchEvent) => {
-    if (startY === null) return;
-
-    const endY = e.changedTouches[0].clientY;
-    const deltaY = endY - startY;
-
-    if (Math.abs(deltaY) > 30) {
-      setSwiped(deltaY > 0 ? "down" : "up");
-    }
-
-    setStartY(null);
-  };
   return (
-    <div className="my-0 mx-auto xl:w-[80%] xl:h-[80%] w-full h-full xl:rounded-xl overflow-hidden  bg-white max-h-screen ">
+    <div className="my-0 mx-auto xl:w-[80%] xl:h-[80vh] w-full xl:rounded-xl bg-white   h-screen overflow-hidden ">
       <div
-        className={` grid xl:grid-cols-[2fr_1fr] xl:grid-rows-[auto_1fr_1fr] grid-cols-1 grid-rows-[auto_auto_1fr]  h-full `}
+        className={` grid xl:grid-cols-[2fr_1fr] xl:grid-rows-[auto_1fr_1fr] grid-cols-1 grid-rows-[auto_auto_1fr]  h-full  `}
       >
-        <div className="order-2 xl:col-start-1 xl:row-start-1  xl:row-end-4 w-full flex justify-center items-center  relative ">
+        {/* gallery */}
+        <div className="order-2 xl:col-start-1 xl:row-start-1  xl:row-end-4 w-full flex justify-center items-center  relative  xl:max-h-[80vh]  ">
+          {/* photos */}
           <div
             className="bg-gray-200 w-full h-full flex justify-center items-center "
             onTouchStart={() => {
@@ -53,6 +37,7 @@ const PostView = ({ hideOverlay }: { hideOverlay?: Function }) => {
               />
             ))}
           </div>
+          {/* buttons mobile */}
           <div
             className={
               "xl:hidden absolute  right-0 bottom-1/3 transition-opacity " +
@@ -68,67 +53,27 @@ const PostView = ({ hideOverlay }: { hideOverlay?: Function }) => {
           </div>
         </div>
 
-        <div className=" order-1 p-4">
+        {/* header and description */}
+        <div className=" order-1 p-4 ">
           <div className=" flex items-start justify-between ">
             <PostHeader />
             {hideOverlay && <button onClick={() => hideOverlay()}>X</button>}
           </div>
-          <div>{post.data.description}</div>
+          <div className="overflow-y-auto max-h-[20vh] ">
+            {post.data.description}
+          </div>
         </div>
 
-        <div className="order-3 xl:row-start-2 xl:row-end-4 flex flex-col justify-between ">
+        {/* buttons */}
+        <div
+          className={`order-3 xl:row-start-2 xl:row-end-4 flex flex-col overflow-hidden  `}
+        >
+          {/* buttons xl */}
           <div className="  px-4 py-2 xl:flex flex-row-reverse items-start gap-2  hidden">
             <LikeButton />
             <ShareButton />
           </div>
-          <div
-            className={`w-full  flex-grow  flex flex-col  bg-white overflow-hidden  
-             `}
-            onTouchStart={handleTouchStart}
-            onTouchEnd={handleTouchEnd}
-          >
-            <div
-              className="font-semibold text-center  rounded-b-xl py-1 px-2 mt-auto"
-              onClick={() => {
-                setSwiped((prev) => (prev === "up" ? "down" : "up"));
-              }}
-            >
-              Comments {post.data.commentNumber}
-              <div className="h-1 w-12 mx-auto rounded-full bg-black self-center"></div>
-            </div>
-            <div
-              className={`overflow-y-scroll  xl:flex flex-col  justify-start  xl:max-h-screen xl:h-full max-h-[60vh] h-full ${
-                swiped === "up" ? "flex" : "hidden"
-              }`}
-            >
-              <div
-                className="overflow-y-scroll xl:max-h-full h-full  "
-                onTouchStart={(e) => {
-                  e.stopPropagation();
-                }}
-                onTouchEnd={(e) => {
-                  e.stopPropagation();
-                }}
-              >
-                <div className="px-2">comment</div>
-
-                <div className="px-2">comment</div>
-                <div className="px-2">comment</div>
-                <div className="px-2">comment</div>
-              </div>
-              <div className="w-full">
-                <input
-                  type="search"
-                  style={{ WebkitAppearance: "none", MozAppearance: "none" }}
-                  autoComplete="off"
-                  autoCorrect="off"
-                  spellCheck="false"
-                  className="block w-full border-t-[1px] p-2 rounded-xl "
-                  placeholder="Comment..."
-                />
-              </div>
-            </div>
-          </div>
+          <Comments setSwiped={setSwiped} swiped={swiped} />
         </div>
       </div>
     </div>
---
+++ frontend/src/sections/profile/posts_components/post_components/post_gallery_components/post_view_components/Comments.tsx
@@ -0,0 +1,120 @@
+import { useState } from "react";
+import { PostContextType, usePostContext } from "../../../Post";
+
+const Comments = ({
+  swiped,
+  setSwiped,
+}: {
+  swiped: "up" | "down";
+  setSwiped: React.Dispatch<React.SetStateAction<"up" | "down">>;
+}) => {
+  const post: PostContextType = usePostContext();
+  if (!post) return null;
+  const [startY, setStartY] = useState<number | null>(null);
+  console.log("a");
+  const handleTouchStart = (e: React.TouchEvent) => {
+    setStartY(e.touches[0].clientY);
+  };
+
+  const handleTouchEnd = (e: React.TouchEvent) => {
+    if (startY === null) return;
+
+    const endY = e.changedTouches[0].clientY;
+    const deltaY = endY - startY;
+
+    if (Math.abs(deltaY) > 30) {
+      setSwiped(deltaY > 0 ? "down" : "up");
+    }
+
+    setStartY(null);
+  };
+  return (
+    <div
+      className={`flex-grow grid grid-rows-[auto_1fr] overflow-hidden  `}
+      onTouchStart={handleTouchStart}
+      onTouchEnd={handleTouchEnd}
+    >
+      {/* comments header */}
+      <div
+        className={`font-semibold text-center w-full rounded-b-xl xl:static  py-1 px-2 mt-auto bg-white ${
+          swiped === "up" ? "" : "fixed"
+        } bottom-0 `}
+        onClick={() => {
+          setSwiped((prev) => (prev === "up" ? "down" : "up"));
+        }}
+      >
+        Comments {post.data.commentNumber}
+        <div className="h-1 w-12 mx-auto rounded-full bg-black self-center"></div>
+      </div>
+
+      {/* comments */}
+      <div
+        className={` grid-rows-[1fr_auto] xl:grid overflow-hidden
+            ${swiped === "up" ? "grid" : "hidden"}`}
+      >
+        <div
+          className="overflow-y-scroll"
+          onTouchStart={(e) => {
+            e.stopPropagation();
+          }}
+          onTouchEnd={(e) => {
+            e.stopPropagation();
+          }}
+        >
+          <div className="px-2">comment</div>
+          <div className="px-2">comment</div>
+          <div className="px-2">comment</div>
+          <div className="px-2">comment</div>
+          <div className="px-2">comment</div>
+          <div className="px-2">comment</div>
+          <div className="px-2">comment</div>
+          <div className="px-2">comment</div>
+          <div className="px-2">comment</div>
+          <div className="px-2">comment</div>
+          <div className="px-2">comment</div>
+          <div className="px-2">comment</div>
+          <div className="px-2">comment</div>
+          <div className="px-2">comment</div>
+          <div className="px-2">comment</div>
+          <div className="px-2">comment</div>
+          <div className="px-2">comment</div>
+          <div className="px-2">comment</div>
+          <div className="px-2">comment</div>
+          <div className="px-2">comment</div>
+          <div className="px-2">comment</div>
+          <div className="px-2">comment</div>
+          <div className="px-2">comment</div>
+          <div className="px-2">comment</div>
+          <div className="px-2">comment</div>
+          <div className="px-2">comment</div>
+          <div className="px-2">comment</div>
+          <div className="px-2">comment</div>
+          <div className="px-2">comment</div>
+          <div className="px-2">comment</div>
+
+          <div className="px-2">comment</div>
+          <div className="px-2">comment</div>
+          <div className="px-2">comment</div>
+          <div className="px-2">comment</div>
+          <div className="px-2">comment</div>
+          <div className="px-2">comment</div>
+          <div className="px-2  bg-black">comment</div>
+        </div>
+        {/* input */}
+        <div className="">
+          <input
+            type="search"
+            style={{ WebkitAppearance: "none", MozAppearance: "none" }}
+            autoComplete="off"
+            autoCorrect="off"
+            spellCheck="false"
+            className="block w-full border-t-[1px] p-2 rounded-xl "
+            placeholder="Comment..."
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Comments;
